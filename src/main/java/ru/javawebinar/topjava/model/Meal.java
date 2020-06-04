79074f821b9fb1530c6bec632cc405331637b78b
GrNata <gr.natalij@gmail.com>
2020-06-04 12:53:20 +0300

    prepare to HW0 patch home work

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,15 +1,16 @@
 package ru.javawebinar.topjava.model;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public class UserMeal {
+public class Meal {
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+    public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -26,4 +27,6 @@ public class UserMeal {
     public int getCalories() {
         return calories;
     }
+
+    public LocalDate getDate()  {   return dateTime.toLocalDate();  }
 }
