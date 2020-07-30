4d075a8f6ddab5e8372e4f9cbcfd5f0d4cb2b97a
GrNata <gr.natalij@gmail.com>
2020-07-30 10:16:55 +0300

    8 0 2 rename

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -24,7 +24,7 @@ class InMemoryAdminRestControllerSpringTest {
     private InMemoryUserRepository repository;
 
     @BeforeEach
-    void setUp() throws Exception {
+    void setup() throws Exception {
         repository.init();
     }
 
